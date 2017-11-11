9a8f17a3b82e2dfd64a3f3c7d6e87a4e6957844e
zbhatt <zanky_15@hotmail.com>
2017-11-11 11:59:40 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,2 +1,18 @@
+
 # Developing-Data-products-week4
 Final Project
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Developing Data Products Week4"),
+  
+  
+    
+    # Show a plot of the generated distribution
+  ui <- basicPage(
+    plotOutput("plot1", click = "plot_click"),
+    verbatimTextOutput("info")
+  )
+    )
+  )
